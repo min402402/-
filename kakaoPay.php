37560a2b2aced8d16518bf43ee26d58243537228
zkdlwyu <39186870+zkdlwyu@users.noreply.github.com>
2019-05-12 13:31:10 +0900

    Add files via upload

--- kakaoPay.php
+++ kakaoPay.php
@@ -1,6 +1,4 @@
-<?php
-    echo "test";
-?>
+<?php $a = $_POST['pay']; ?>
 
 <html>
 <head>
@@ -9,7 +7,10 @@
     <script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-1.1.5.js"></script>
 </head>
 <body>
+
+
 <script>
+
     var IMP = window.IMP; // 생략가능
 
     IMP.init('imp88648066'); // 'iamport' 대신 부여받은 "가맹점 식별코드"를 사용
@@ -19,13 +20,13 @@
     pay_method : 'card',
     merchant_uid : 'merchant_' + new Date().getTime(),
     name : '주문명:결제테스트',
-    amount : 14000,
+    amount : '<?php echo $a; ?>',
     buyer_email : 'iamport@siot.do',
     buyer_name : '구매자이름',
     buyer_tel : '010-1234-5678',
     buyer_addr : '서울특별시 강남구 삼성동',
     buyer_postcode : '123-456',
-    m_redirect_url : 'bulletaxi.dothome.co.kr/ssibal.php'
+    m_redirect_url : 'bulletaxi.cafe24.com/jh/dv_reserve.php'
 
 }, function(rsp) {
     if ( rsp.success ) {
@@ -34,6 +35,8 @@
         msg += '상점 거래ID : ' + rsp.merchant_uid;
         msg += '결제 금액 : ' + rsp.paid_amount;
         msg += '카드 승인번호 : ' + rsp.apply_num;
+
+        window.location.href = '../jh/dv_complete.php';
     } else {
         var msg = '결제에 실패하였습니다.';
         msg += '에러내용 : ' + rsp.error_msg;
